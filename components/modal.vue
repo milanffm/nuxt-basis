763080dad3bc67b074b590f6909c90822126892c
Milan Peters <m.peters@graphicon.de>
2019-09-27 12:04:10 +0200

    test vs code and refactor two files

--- components/modal.vue
+++ components/modal.vue
@@ -1,94 +1,89 @@
 <template>
-	<transition name="modal">
-		<div class="modal-mask">
-			<div class="modal-wrapper" @click="closeOutside($event)">
-				<div class="modal-container">
-					<div class="modal-content">
-						<div class="modal-head">
-							<slot name="head">
-							</slot>
-						</div>
-						<div class="modal-body">
-							<slot name="body">
-							</slot>
-						</div>
-					</div>
-				</div>
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper" @click="closeOutside($event)">
+                <div class="modal-container">
+                    <div class="modal-content">
+                        <div class="modal-head">
+                            <slot name="head"></slot>
+                        </div>
+                        <div class="modal-body">
+                            <slot name="body"></slot>
+                        </div>
+                    </div>
+                </div>
 
-				<div class="modal-close" @click="$emit('close')">
-				</div>
-			</div>
-		</div>
-	</transition>
+                <div class="modal-close" @click="$emit('close')"></div>
+            </div>
+        </div>
+    </transition>
 </template>
 
 <script>
 export default {
-	name: 'modal',
-	methods: {
-		closeOutside(e){
-			if (e.target.classList.contains('modal-wrapper')) {
-				this.$emit('close');
-			}
-		}
-	}
-}
+    name: "modal",
+    methods: {
+        closeOutside(e) {
+            if (e.target.classList.contains("modal-wrapper")) {
+                this.$emit("close");
+            }
+        }
+    }
+};
 </script>
 
 <style lang="scss" scoped>
-	.modal-mask {
-		position: fixed;
-		z-index: 9998;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		background-color: rgba(0, 0, 0, .9);
-		display: table;
-		transition: opacity .5s ease;
-	}
+.modal-mask {
+    position: fixed;
+    z-index: 9998;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.9);
+    display: table;
+    transition: opacity 0.5s ease;
+}
 
-	.modal-wrapper {
-		display: table-cell;
-		vertical-align: middle;
-		@include padding(5vh null);
+.modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+    @include padding(5vh null);
+}
 
-	}
+.btn {
+    margin-top: 20px;
+}
+.modal-container {
+    width: 80%;
+    margin: 0px auto;
+    padding: 5vw;
+    transition: all 0.5s ease;
+    overflow: auto;
+    display: table;
+    text-align: center;
+    max-width: 900px;
+    background: $white;
+    border-radius: 25px;
+}
 
-	.btn {
-		margin-top: 20px;
-	}
-	.modal-container {
-		width: 80%;
-		margin: 0px auto;
-		padding: 5vw;
-		transition: all .5s ease;
-		overflow: auto;
-		display: table;
-		text-align: center;
-		max-width: 900px;
-		background: $white;
-		border-radius: 25px;
-	}
+.modal-content {
+    max-height: 69vh;
+    overflow: auto;
+}
+.modal-close {
+    @include icon("icon-close");
+    @include position(absolute, 1.5vh 5% null null);
+    top: 0.8vh;
+    right: 63px;
+    cursor: pointer;
+    &:before {
+        color: $white;
+        font-size: $icon-big-font-size;
+    }
+}
 
-	.modal-content {
-		max-height: 69vh;
-		overflow: auto;
-	}
-	.modal-close {
-		@include icon('icon-close');
-		@include position(absolute, 1.5vh 5% null null);
-		top: .80vh;
-		right: 63px;
-		cursor: pointer;
-		&:before {
-			color: $white;
-			font-size: $icon-big-font-size;
-		}
-	}
-
-
-	/*
+/*
 	 * The following styles are auto-applied to elements with
 	 * transition="modal" when their visibility is toggled
 	 * by Vue.js.
@@ -97,28 +92,30 @@ export default {
 	 * these styles.
 	 */
 
-	.modal-enter-active, .modal-leave-active {
-		transition: opacity .5s;
-	}
-	.modal-enter, .modal-leave-active {
-		opacity: 0;
-	}
+.modal-enter-active,
+.modal-leave-active {
+    transition: opacity 0.5s;
+}
+.modal-enter,
+.modal-leave-active {
+    opacity: 0;
+}
 
-	.modal-enter .modal-container,
-	.modal-leave-active .modal-container {
-		-webkit-transform: scale(1.1);
-		transform: scale(1.1);
-	}
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+}
 
-	@media all and (max-width: $small-screen) {
-		.modal-wrapper {
-			@include padding(2vh null);
-		}
-		.modal-container {
-			padding: 3%;
-		}
-		.modal-body {
-			@include hyphens();
-		}
-	}
+@media all and (max-width: $small-screen) {
+    .modal-wrapper {
+        @include padding(2vh null);
+    }
+    .modal-container {
+        padding: 3%;
+    }
+    .modal-body {
+        @include hyphens();
+    }
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,54 @@
 <template>
-	<section class="container">
-		<div>
-			<h1 class="title">Home</h1>
-			<h2 class="subtitle">basis nuxt site</h2>
-			<img v-for="(image, index) in images " v-on:click="showLightBox(index)" :src="image">
-		</div>
-		<light-box
-			v-if="showModal"
-			@close="showModal = false"
-			v-bind:images="images"
-			v-bind:currentGalleryIndex="galleryIndex"
-		></light-box>
-	</section>
+    <section class="container">
+        <div>
+            <h1 class="title">Home</h1>
+            <h2 class="subtitle">basis nuxt site</h2>
+            <img v-for="(image, index) in images " v-on:click="showLightBox(index)" :src="image" />
+        </div>
+        <light-box
+            v-if="showModal"
+            @close="showModal = false"
+            v-bind:images="images"
+            v-bind:currentGalleryIndex="galleryIndex"
+        ></light-box>
+    </section>
 </template>
 <script>
-
-import {mapState} from 'vuex';
+import { mapState } from "vuex";
 
 export default {
-	data() {
-		return {
-			showModal: false,
-			images: ['https://via.placeholder.com/150','https://via.placeholder.com/250','https://via.placeholder.com/350'],
-			galleryIndex: null
-		};
-	},
-	computed: {
-		...mapState({
-			posts: state => state.post.posts
-		})
-	},
-	mounted() {
-		if (this.$store.state.post.posts.length < 1) {
-			return this.$store.dispatch('post/getPosts');
-		}
-	},
-	methods: {
-		showLightBox(index) {
-			this.galleryIndex = index;
-			this.showModal = true;
-		}
-	},
+    data() {
+        return {
+            showModal: false,
+            images: [
+                "https://via.placeholder.com/150",
+                "https://via.placeholder.com/250",
+                "https://via.placeholder.com/350"
+            ],
+            galleryIndex: null
+        };
+    },
+    computed: {
+        ...mapState({
+            posts: state => state.post.posts
+        })
+    },
+    mounted() {
+        if (this.$store.state.post.posts.length < 1) {
+            return this.$store.dispatch("post/getPosts");
+        }
+    },
+    methods: {
+        showLightBox(index) {
+            this.galleryIndex = index;
+            this.showModal = true;
+        }
+    }
 };
 </script>
 
 <style lang="scss" scoped>
-	h1 {
-		background: $btn-color;
-	}
+h1 {
+    background: $btn-color;
+}
 </style>
