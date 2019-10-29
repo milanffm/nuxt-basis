a1e62c104045bdbfad483c7058b0b4f8eed5554e
Milan Peters <m.peters@graphicon.de>
2019-10-29 14:56:34 +0100

    add eslint and prepare vor VsCode

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,36 +1,43 @@
 <template>
-	<section class="container">
-		<div>
-			<h1 class="title">Kontakt</h1>
-			<p>Kontakt Seite</p>
-			<p @click="showModal = true">show Modal</p>
-		</div>
-		<modal v-show="showModal" @close="showModal = false">
-			<div slot="body">
-				<p>body</p>
-			</div>
-		</modal>
-	</section>
+    <section class="container">
+        <div>
+            <h1 class="title">Kontakt</h1>
+            <p>Kontakt Seite</p>
+            <p @click="showModal = true">show Modal</p>
+        </div>
+        <modal
+            v-show="showModal"
+            @close="showModal = false"
+        >
+            <div slot="body">
+                <p>modal body</p>
+                <p>modal body</p>
+                <p>modal body</p>
+                <p>modal body</p>
+                <p>modal body</p>
+                <p>modal body</p>
+                <p>modal body</p>
+                <p>modal body</p>
+                <p>modal body</p>
+            </div>
+        </modal>
+    </section>
 </template>
 
 <script>
-import { mapState } from 'vuex';
 
 export default {
-	data() {
-		return {
-			showModal: false
-		};
-	},
-	components: {},
-	computed: {},
-	mounted() {},
-	methods: {}
+    data() {
+        return {
+            showModal: false
+        };
+    },
+    components: {},
+    computed: {},
+    mounted() { },
+    methods: {}
 };
 </script>
 
 <style lang="scss">
-	div {
-		background: #fff;
-	}
 </style>
